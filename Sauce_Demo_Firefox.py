ff412bea2a982446213b52ad5cc7415d98687152
wofai <wofai6334@gmaail.com>
2024-06-29 18:12:07 +0100

    Add to Cart

---
+++ Sauce_Demo_Firefox.py
@@ -0,0 +1,61 @@
+from modulefinder import Module
+
+import select
+from selenium import webdriver
+import driver as driver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+import time
+
+# Initialize the WebDriver
+driver = webdriver.Firefox()
+time.sleep(4)
+driver.maximize_window()
+
+# Navigate to the URL
+driver.get("https://www.saucedemo.com/")
+time.sleep(10)
+
+# Input Credentials
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+time.sleep(2)
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+time.sleep(2)
+driver.find_element(By.ID, "login-button").click()
+time.sleep(3)
+
+driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select').click()
+driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select').send_keys("Price (high to low)")
+time.sleep(5)
+
+# Add_Product_to_Cart
+driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
+time.sleep(3)
+driver.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
+time.sleep(3)
+
+# My_Cart
+driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+time.sleep(3)
+driver.find_element(By.NAME, "checkout").click()
+time.sleep(3)
+
+# Checkout_Your_Information
+driver.find_element(By.ID, "first-name").send_keys("Ralia")
+time.sleep(3)
+driver.find_element(By.ID, "last-name").send_keys("Odinga")
+time.sleep(3)
+driver.find_element(By.NAME, "postalCode").send_keys("12345")
+time.sleep(5)
+driver.find_element(By.ID, "continue").click()
+time.sleep(8)
+
+# Checkout: Overview
+driver.find_element(By.ID, "finish").click()
+time.sleep(6)
+
+# Hand_Bogger_Logout
+driver.find_element(By.ID, "react-burger-menu-btn").click()
+time.sleep(2)
+driver.find_element(By.ID, "logout_sidebar_link").click()
+time.sleep(3)

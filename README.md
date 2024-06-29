f6251220040cbfdfef64d7eaa1dcf044fc4bc713
wofai <wofai6334@gmaail.com>
2024-06-29 18:48:23 +0100

    buy product

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Sauce_Demo
 Run_Selenium
 
-Discription of Sauce Demo 
+Description of Sauce Demo 
 The "Sauce Demo" site mimics an e-commerce platform, providing a range of features and functionalities typical of online retail sites. Here are some key aspects of the "Sauce Demo" site:
 User Login, login page where users can enter credentials to access the platform. Once logged in, users can view a list of products, complete with images, descriptions, and prices. Product Details, Provides more detailed information. Shopping Cart, shopping cart where users can add or remove items. Checkout Process, The checkout process involves several steps, including entering shipping information, selecting payment methods, and confirming orders. 
 
--- SAUCE_DEMO_AUTOMATION_TEST.py
+++ SAUCE_DEMO_AUTOMATION_TEST.py
@@ -59,4 +59,110 @@ time.sleep(2)
 driver.find_element(By.ID, "logout_sidebar_link").click()
 time.sleep(3)
 
+
+# Edge Browser
+
+# Initialize the WebDriver
+driver = webdriver.Edge()
+time.sleep(4)
+driver.maximize_window()
+
+# Navigate to the URL
+driver.get("https://www.saucedemo.com/")
+time.sleep(10)
+
+# Input Credentials
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+time.sleep(2)
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+time.sleep(2)
+driver.find_element(By.ID, "login-button").click()
+time.sleep(3)
+
+# Add_Product_to_Cart
+driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
+time.sleep(3)
+driver.find_element(By.ID, "add-to-cart-sauce-labs-fleece-jacket").click()
+time.sleep(3)
+
+# My_Cart
+driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+time.sleep(3)
+driver.find_element(By.NAME, "checkout").click()
+time.sleep(3)
+
+# Checkout_Your_Information
+driver.find_element(By.ID, "first-name").send_keys("Mikky")
+time.sleep(3)
+driver.find_element(By.ID, "last-name").send_keys("Mouse")
+time.sleep(3)
+driver.find_element(By.NAME, "postalCode").send_keys("5443")
+time.sleep(5)
+driver.find_element(By.ID, "continue").click()
+time.sleep(8)
+
+# Checkout: Overview
+driver.find_element(By.ID, "finish").click()
+time.sleep(6)
+
+# Hand_Bugger_Logout
+driver.find_element(By.ID, "react-burger-menu-btn").click()
+time.sleep(2)
+driver.find_element(By.ID, "logout_sidebar_link").click()
+time.sleep(3)
+
+# Fire Fox Browser
+# Initialize the WebDriver
+driver = webdriver.Firefox()
+time.sleep(4)
+driver.maximize_window()
+
+# Navigate to the URL
+driver.get("https://www.saucedemo.com/")
+time.sleep(10)
+
+# Input Credentials
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+time.sleep(2)
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+time.sleep(2)
+driver.find_element(By.ID, "login-button").click()
+time.sleep(3)
+
+driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select').click()
+driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select').send_keys("Price (high to low)")
+time.sleep(5)
+
+# Add_Product_to_Cart
+driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
+time.sleep(3)
+driver.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
+time.sleep(3)
+
+# My_Cart
+driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+time.sleep(3)
+driver.find_element(By.NAME, "checkout").click()
+time.sleep(3)
+
+# Checkout_Your_Information
+driver.find_element(By.ID, "first-name").send_keys("Penny")
+time.sleep(3)
+driver.find_element(By.ID, "last-name").send_keys("Wise")
+time.sleep(3)
+driver.find_element(By.NAME, "postalCode").send_keys("2443")
+time.sleep(5)
+driver.find_element(By.ID, "continue").click()
+time.sleep(8)
+
+# Checkout: Overview
+driver.find_element(By.ID, "finish").click()
+time.sleep(6)
+
+# Hand_Bugger_Logout
+driver.find_element(By.ID, "react-burger-menu-btn").click()
+time.sleep(2)
+driver.find_element(By.ID, "logout_sidebar_link").click()
+time.sleep(5)
+
 driver.quit()
